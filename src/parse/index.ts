c741ec3db2ccf91e0c3e106c593b45fe28b53120
Dominik Bošnjak <bosnjakinimod@gmail.com>
2026-04-26 07:44:08 +0200

    aded src/parse/index.ts

---
+++ src/parse/index.ts
@@ -0,0 +1,7 @@
+import { json } from "./json";
+import { jsonc } from "./jsonc";
+import { jsonl } from "./jsonl";
+import { text } from "./text";
+
+export { json, jsonc, jsonl, text };
+export default { json, jsonc, jsonl, text };
